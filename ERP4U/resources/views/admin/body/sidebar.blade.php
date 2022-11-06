c5f94a06fe7d2fa45276a549a42d8f714280c7cc
Daniel Vicente <danielvice988@gmail.com>
2022-11-06 20:52:44 +0000

    Initial commit

---
+++ ERP4U/resources/views/admin/body/sidebar.blade.php
@@ -0,0 +1,101 @@
+ <div class="vertical-menu">
+
+                <div data-simplebar class="h-100">
+
+                    <!-- User details -->
+                
+
+                    <!--- Sidemenu -->
+                    <div id="sidebar-menu">
+                        <!-- Left Menu Start -->
+                        <ul class="metismenu list-unstyled" id="side-menu">
+                            <li class="menu-title">Menu</li>              
+                            <li>
+                                <a href="javascript: void(0);" class="has-arrow waves-effect">
+                                    <i class="ri-tools-fill"></i>
+                                    <span>Settings</span>
+                                </a>
+                                <ul class="sub-menu" aria-expanded="false">
+                                    <li><a href="geral.html">General</a></li>
+                                    <li><a href="l10n-read.html">Localization</a></li>
+                                    <li><a href="Company.html">Company</a></li>
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);" class="has-arrow waves-effect">
+                                    <i class="ri-table-2"></i>
+                                    <span>Common Tables</span>
+                                </a>    
+                                <ul class="sub-menu" aria-expanded="false">
+                                    <li><a href="series.html">Series</a></li>
+                                    <li><a href="taxrates.html">Tax Rates</a></li>
+                                    <li><a href="units.html">Units</a></li>
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);" class="has-arrow waves-effect">
+                                    <i class="ri-file-text-line"></i>
+                                    <span>Documents</span>
+                                </a>
+                                <ul class="sub-menu" aria-expanded="true">
+                                    <li>
+                                        <a href="javascript: void(0);" class="has-arrow">Entry Documents</a>
+                                        <ul class="sub-menu" aria-expanded="true">
+                                            <li><a href="ed1.html">Quotation Request</a></li>
+                                            <li><a href="ed2.html">Purchase Order</a></li>
+                                           <li><a href="ed3.html">Goods Receipt</a></li>
+                                           <li><a href="ed4.html">??????</a></li>
+                                        </ul>
+                                    </li>
+
+                                    <li>
+                                        <a href="javascript: void(0);" class="has-arrow">Output Documents</a>
+                                        <ul class="sub-menu" aria-expanded="true">
+                                            <li><a href="ds1.html">?????</a>????</li>
+                                            <li><a href="ds2.html">?????</a>????</li>
+                                            <li><a href="ds3.html">?????</a>????</li>
+                                            <li><a href="ds4.html">?????</a>????</li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li class="menu-title">Infrastructure</li>
+
+                            <li>
+                                <a href="javascript: void(0);" class="has-arrow waves-effect">
+                                    <i class="ri-account-circle-line"></i>
+                                    <span>ERP4U Ticket Line</span>
+                                </a>
+                                <ul class="sub-menu" aria-expanded="false">
+                                    <li><a href="????.html">Create a Ticket</a></li>
+                                    <li><a href="????.html">Messages</a></li>
+                                    <li><a href="????.html">Online Assistance</a></li>
+                                    <li><a href="????.html">Updates</a></li>
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);" class="has-arrow waves-effect">
+                                    <i class="ri-profile-line"></i>
+                                    <span>Utility</span>
+                                </a>
+                                <ul class="sub-menu" aria-expanded="false">
+                                    <li><a href="???">Calculator</a></li>
+                                    <li><a href="???">Browser</a></li>
+                                    <li><a href="???">Alexa</a></li>
+                                </ul>
+                            </li>
+
+                           
+
+                            
+                         
+
+                        </ul>
+                    </div>
+                    <!-- Sidebar -->
+                </div>
+            </div>
